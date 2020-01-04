bd568be42345b3092ebe8c354f17cdb708fb4e06
Miguelro123 <969@holbertonschool.com>
2020-01-04 02:43:05 +0000

    web debbuging

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,16 +2,16 @@
 # Fabric script to generate .tgz archive file
 
 from fabric.api import *
-from time import strftime
+from time import strftime as ti
 
 
 def do_pack():
     """Fabric script to compress files in web_static"""
-    ver = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static".format(ver))
- 
-   except:
-       return None
-   return ("versions/web_static_{}.tgz".format(ver))
+    local("mkdir -p versions")
+    ver = ti("%Y%m%d%H%M%S")
+    arc = local("tar -cvzf versions/web_static_{}.tgz web_static".format(ver))
+
+    if arc is None:
+        return None
+    else:
+        return ("versions/web_static_{}".format(ver))
