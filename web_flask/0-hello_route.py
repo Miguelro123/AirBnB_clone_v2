6a1513546478c050de13160007c3d68ec8d1cd1b
Miguelro123 <969@holbertonschool.com>
2020-01-22 17:56:43 +0000

    starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" Python script that starts a flask application on 0.0.0.0 port 5000 """
+""" Write a script that starts a Flask web
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route("/")
+
+@app.route("/", strict_slashes=False)
 def index():
+    """ Print the message """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
