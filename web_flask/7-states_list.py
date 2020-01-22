cd36b46d9cce4f7a6da8a5d2ab208fdf0dad301e
Miguelro123 <969@holbertonschool.com>
2020-01-22 23:50:20 +0000

    starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -17,9 +18,9 @@ def teardown(self):
 @app.route('/states_list')
 def states_list():
     """ Print the number """
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
