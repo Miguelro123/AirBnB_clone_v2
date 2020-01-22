f2b627708bc8477f3bfdf8027dd1e9e9ddafd54b
Miguelro123 <969@holbertonschool.com>
2020-01-22 17:49:31 +0000

    starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python script that starts a flask application on 0.0.0.0 port 5000
+""" Python script that starts a flask application on 0.0.0.0 port 5000 """
 from flask import Flask
 
 app = Flask(__name__)
