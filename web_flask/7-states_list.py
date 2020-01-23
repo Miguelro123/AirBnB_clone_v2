491ce8687a6f6c6e36be7132041ebbcc9647eb03
Miguelro123 <969@holbertonschool.com>
2020-01-23 00:12:28 +0000

    starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
-
-from flask import Flask, render_template
+""" Shows a message """
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
-def teardown(self):
-    """Close storage session"""
+def teardown_st(self):
+    """ close storage """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """display a HTML page with all states and cities"""
-    states = storage.all(State)
+    """ Print the number """
+    states = storage.all('State').values()
     return render_template('7-states_list.html', states=states)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
